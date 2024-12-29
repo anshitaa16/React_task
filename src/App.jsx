1a37204c3ef89f92dca6479c03a0724ddbc7c26c
anshitaa16 <142668368+anshitaa16@users.noreply.github.com>
2024-12-29 22:11:03 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import LoginPage from './components/login';
 import HomePage from './components/home';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={
           <ProtectedRoute><HomePage /></ProtectedRoute>
         } />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
